e8657712f9902adba45f05899b8de0935c3ece61
Colm Le Gear <colm.legear@gmail.com>
2019-04-01 15:43:13 +0100

    Create advertise_beacon.sh

---
+++ advertise_beacon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# uuid: E20A39F4-73F54BC4-A12F-17D1AD07A96
+
+sudo hciconfig hci0 up
+sudo hciconfig hci0 leadv 3
+sudo hciconfig hci0 noscan
+sudo hcitool -i hci0 cmd 0x08 0x0008 1E 02 01 1A 1A FF 4C 00 02 15 E2 0A 39 F4 73 F5 4B C4 A1 2F 17 D1 AD 07 A9 61 00 00 $1 $2 C8 00
